bd4b4b7592432eeab177827c1e33466fcd696b10
NeupaneManan <neupanemanan.2003@gmail.com>
2023-09-24 22:20:09 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const a=10;
+const b=20;
+const name="Manan"
+const sum= a+b;
+console.log(sum, name);
+
+
+//Array
+
+//const numbers=[10 , 20 , 30];
+//console.log(numbers[1]);
+//console.log(numbers[0]);
+
+//to find length of array
+
+//console.log(numbers.length);
+
+//Object
+
+const person = {
+    name: "Manan",
+    age : 21,
+}
+
+console.log("name: " + person.name);
+console.log("age:" + person.age);
